c4fbc341b5964089344a6b66ab90a16325a15a69
Dominic <tomas.schlueter@gmail.com>
2025-11-05 17:22:03 +0100

    Done for the day

--- main.cpp
+++ main.cpp
@@ -3,16 +3,33 @@
 #include <filesystem>
 #include <fstream>
 using namespace std;
+bool DevMode;
 
 string getfolder() {
-    filesystem::path Ordnerpath = filesystem::current_path() / "Ordner";
-    string Ordnerstring = Ordnerpath.string();
-    return Ordnerstring;
+    filesystem::path Ordnerpath = filesystem::current_path();   //Ordnerpath findet den ordnerpfad und setzt Ordnerpath als diesen
+    string Ordnerstring = Ordnerpath.string();                  //gibt Ordnerstring mit dem selben information wie ordnerpath als string an
+    cout << Ordnerstring << endl;                               //gibt den string aus
+    return Ordnerstring;                                        //gibt als die Funktionswerk Ordnerstring aus
+}
+void getConfig() {
+    ifstream config(getfolder()+"\\Config.txt");              //öffnet config
+    if (!config.is_open()) {
+        cout << "Error in opening config file" << endl;            //error wenn config nicht gefunen werden kann
+    }
+    config.ignore(100, '=');                            //geht bis zu 100 zeichen nach dem nächsten =
+    config >> DevMode;
+    if (DevMode) {
+        cout << DevMode << endl;                                //wenn devmode=1 ist gibt es 1 aus
+    }
+
+    config.close();
 }
 
 int main() {
+    ifstream FileC;                         //noch workinprogress
+    getConfig();
+    FileC.open(getfolder()+"");
 
 
-
-
+    return 0;
 }

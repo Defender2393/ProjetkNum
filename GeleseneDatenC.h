3781b870886f5fdb15e53778775651e7f89c4b8d
Dominic <tomas.schlueter@gmail.com>
2025-11-19 14:52:50 +0100

    AAAAA

--- GeleseneDatenC.h
+++ GeleseneDatenC.h
@@ -8,30 +8,41 @@
 #include <array>
 
 class GeleseneDatenC {
-    std::pmr::vector<std::array <float, 3>> MainDatax;
-    std::pmr::vector<std::array <float, 3>> MainDatay;
-    std::pmr::vector<std::array <float, 3>> MainDataz;
-    public:
-    GeleseneDatenC(int* x, float** arrx,int y, float** arry){
-        for (int i = 0; i < x[0]; i++) {
-            MainDatax.push_back(0);
-            MainDatax[i][0] = arrx[i][0];
-            MainDatax[i][1] = arrx[i][1];
-            MainDatax[i][2] = arrx[i][2];
+public:
+    int LineNumber[4]{};
+    std::pmr::vector<std::array <float, 12>> LineContent;
+    int kinematicNumber;
+    std::pmr::vector<std::array <float, 8>> kinematicContent;
+    std::array <float, 12> helperLine{} ;
+    std::array <float, 8> helperkinematic{};
+
+    GeleseneDatenC(const int* x, float arrx[1625][12],int y, float arry[1625][8]){
+        for (int j = 0; j < 4; j++) {
+            LineNumber[j] = x[j];
         }
-        for (int i = 0; i < x[1]; i++) {
-            MainDatay.push_back(0);
-            MainDatay[i][0] = arry[i][0];
-            MainDatay[i][1] = arry[i][1];
-            MainDatay[i][2] = arry[i][2];
+        kinematicNumber = y;
+        for (int j = 0; j < LineNumber[0]; j++) {
+            LineContent.push_back(helperLine);
         }
-        for (int i = 0; i < x[3]; i++) {
-            MainDatax.push_back(0);
-            MainDataz[i][0] = arrz[i][0];
-            MainDataz[i][1] = arrz[i][1];
-            MainDataz[i][2] = arrz[i][2];
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0+3*j; i < 3+3*j; i++) {
+                for (int k = 0; k < LineNumber[j]; k++) {
+                    LineContent[i][k] = arrx[i][k];
+                }
+            }
         }
+        for (int j = 0; j < kinematicNumber; j++) {
+            kinematicContent.push_back(helperkinematic);
+            for (int i = 0; i < 8; i++){
+                kinematicContent[j][i] = LineContent[j][i];
+        }
+        }
+
+
+
     };
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 #include <iostream>
 #include <vector>
 #include <array>
-//#include "GeleseneDatenC.h"
+#include <iomanip>
+#include "GeleseneDatenC.h"
+#include <locale>
 using namespace std;
 bool DevMode=false;
 int LineNumber[4];
@@ -15,10 +17,13 @@ int kinematicLine;
 char Character = 'C';
 string FolderNumber;
 array< float , 12> help_Line;
-array<float , 9 > help_kinematic;
+array<float , 8 > help_kinematic;
 vector<array<float , 12>> LineContent;
-vector<array<float ,9 >> kinematicContent;
-
+vector<array<float ,8 >> kinematicContent;
+bool ErrorIsThere = false;
+float PhysVar1;
+float PhysVar2;
+int IsTropfenOrFoam;
 
 string getfolder() {
     filesystem::path Filepath = filesystem::current_path();   //Ordnerpath findet den ordnerpfad und setzt Ordnerpath als diesen
@@ -28,10 +33,17 @@ string getfolder() {
 void getConfig() {
     ifstream config(getfolder()+"\\Config.txt");              //öffnet config
     if (!config.is_open()) {
-        cout << "Error in opening config file" << endl;            //error wenn config nicht gefunen werden kann
+        cout << "Error in opening config file" << endl;      //error wenn config nicht gefunen werden kann
+        ErrorIsThere = true;
     }
     config.ignore(100, '=');                            //geht bis zu 100 zeichen nach dem nächsten =
     config >> DevMode;
+    config.ignore(100, '=');                            //geht bis zu 100 zeichen nach dem nächsten =
+    config >> PhysVar1;
+    config.ignore(100, '=');
+    config >> PhysVar2;
+    config.ignore(100, '=');
+    config >> IsTropfenOrFoam;
     if (DevMode) {
         cout << DevMode << endl;                                //wenn devmode=1 ist gibt es 1 aus
     }
@@ -40,17 +52,19 @@ void getConfig() {
 }
 
 
-int GetData(const string FolderNumberstr) {
+int GetData(const string& FolderNumberstr) {
     ifstream FileC; //erstellt FileC als lesedatei
     for (int z= 0; z < 2 ; z++){
     FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+"\\"+Character);      //öffnet den ordner Fallender Tropfen\ Nummer des Ordners + C oder U in diesem
-    if (!FileC.is_open()&&DevMode==true) {                              //wenn die Datei nicht geöffntet werden kann und der DevMode an ist gibt es einen Error aus
-       cout << "Error in opening file" << endl;
+        FileC.imbue(std::locale::classic());
+        if (!FileC.is_open()&&DevMode==true&&ErrorIsThere==false) {                              //wenn die Datei nicht geöffntet werden kann und der DevMode an ist gibt es einen Error aus
+       cout << "Error in opening file, have you checked weather the Ordner you want opened has the intended Name" << endl;
+        ErrorIsThere = true;
     }
     FileC.ignore(1000, '>');            //geht bis nach dem symbol > in der Datei, hinter diesem befinden sich die Werte
     FileC >> LineNumber[0+3*z]; //holt sich die zahl die hier steht als Int
     FileC.ignore(1000, '(');            //array zum speichern der drei datenmengen
-    if (DevMode){                         //wenn devmode true gibt menge der linien an
+    if (DevMode&&!ErrorIsThere&&z==0){                         //wenn devmode true ist und kein Error existiert gibt menge der linien an
         cout << LineNumber[0] << endl;
     }
     for (int i = 0; i < LineNumber[0]; i++) {                  //wiederholt programm für dateimenge
@@ -79,76 +93,146 @@ int GetData(const string FolderNumberstr) {
     }
     }
     FileC.close();
-    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+"\\lagrangian\\kinematicCloud\\"+Character);
-    FileC.ignore(1000, '(');
-    FileC.seekg(-4, ios::cur);
-    FileC >> kinematicLine;
-    FileC.ignore(10, '(');
+    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+R"(\lagrangian\kinematicCloud\)"+Character);
+    FileC.imbue(std::locale::classic());
+    std::string line;
+
+    // Skip until we find a line that contains ONLY an integer
+    while (std::getline(FileC, line))
+    {
+        std::stringstream ss(line);
+
+        // Try to read an integer
+        if (ss >> kinematicLine)
+        {
+            // Make sure the rest of the line is empty or whitespace
+            std::string remainder;
+            ss >> remainder;
+
+            if (remainder.empty())
+                break;   // We found the line with the count
+        }
+    }
+
+    // If kinematicLine is still 0 or we failed to parse
+    if (kinematicLine <= 0) {
+        std::cout << "Failed to find entry count in kinematicCloud. Last read line was:"
+                  << std::endl << "[" << line << "]" << std::endl;
+        return -1;
+    }
+
+    FileC.ignore(numeric_limits<streamsize>::max(), '(');
     for (int i = 0; i < kinematicLine; i++) {
-        FileC.ignore(10, '(');
+        FileC.ignore(numeric_limits<streamsize>::max(), '(');
         kinematicContent.push_back(help_kinematic);
+
         FileC >> kinematicContent[i][0] >> kinematicContent[i][1] >> kinematicContent[i][2];
     }
     FileC.close();
-    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+"\\lagrangian\\kinematicCloud\\d");
-    FileC.ignore(1000, '(');
+    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+R"(\lagrangian\kinematicCloud\d)");
+    FileC.imbue(std::locale::classic());
+    FileC.ignore(numeric_limits<streamsize>::max(), '(');
     for (int i = 0; i < kinematicLine; i++) {
-        FileC.ignore(10, '(');
-        FileC >> kinematicContent[i][3] >> kinematicContent[i][4] >> kinematicContent[i][5];
+        FileC.ignore(numeric_limits<streamsize>::max(), '(');
+
+       FileC >> kinematicContent[i][3];
     }
     FileC.close();
-    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+"\\lagrangian\\kinematicCloud\\positions");
+    FileC.open(getfolder()+"\\Fallender_Tropfen\\"+FolderNumberstr+R"(\lagrangian\kinematicCloud\positions)");
+    FileC.imbue(std::locale::classic());
     FileC.ignore(1000, '(');
     for (int i = 0; i < kinematicLine; i++) {
-        FileC.ignore(4, '(');
-        FileC >> kinematicContent[i][6] >> kinematicContent[i][7] >> kinematicContent[i][8];
+        FileC.ignore(numeric_limits<streamsize>::max(), '(');
+        FileC >> kinematicContent[i][4] >> kinematicContent[i][5] >> kinematicContent[i][6];
+        FileC.ignore(numeric_limits<streamsize>::max(), ')');
+        FileC >> kinematicContent[i][7];
     }
     return LineNumber[0];
 }
 int main() {
         getConfig();
-    cout << "Bitte die Nummer des Gewünschten Ordners angeben" << endl;
+    cout << "Bitte die Nummer des Gewuenschten Ordners in korrekter schreibweise angeben" << endl;
     cin >> FolderNumber;
     GetData(FolderNumber);
-    float kinematicContentTransfer[kinematicLine][9];
-    for (int i = 0; i < kinematicLine; i++) {
-        for (int j = 0; j < 9; j++) {
-            kinematicContentTransfer[i][j]=kinematicContent[i][j];
-        }
-    }
-    float LineContentTransfer[LineNumber[0]][11];
-    for (int i = 0; i < LineNumber[0]; i++) {
-        for (int j = 0; j < 11; j++) {
-            LineContentTransfer[i][j]=LineContent[i][j];
-        }
-    }
-    if (DevMode) {
-        cout << "Bestaetige die Ausgabe der lagrangian daten mit 1" << endl;
-        int Devhelp=0;
-        cin >> Devhelp;
-        if (Devhelp==1){
+
+    if (ErrorIsThere==false) {
+        if (IsTropfenOrFoam==1) {
+            float LineContentTransfer[18][12];
+            float kinematicContentTransfer[1][8];
             for (int i = 0; i < kinematicLine; i++) {
-                for (int j = 6; j < 10; j++) {
-                    cout << kinematicContent[i][j] << " ";
+                for (int j = 0; j < 8; j++) {
+                    kinematicContentTransfer[i][j] = kinematicContent[i][j];
                 }
+            }
+            for (int i = 0; i < LineNumber[1]; i++) {
+                for (int j = 0; j < 12; j++) {
+                    LineContentTransfer[i][j] = LineContent[i][j];
+                }
+            }
+            GeleseneDatenC Classname(LineNumber, LineContentTransfer, kinematicLine, kinematicContentTransfer);
+        }
+        //else if (IsTropfenOrFoam==2) {
+          //  float LineContentTransfer[1625][12];
+            //float kinematicContentTransfer[1625][8];
+       // }
+        if (DevMode) {
+            cout << fixed;
+            cout << "Bestaetige die Ausgabe der" << getfolder() <<"\\FallenderTropfen\\"<< FolderNumber <<"\\lagrangian\\kinematicCloud daten mit 1" << endl;
+            int Devhelp=0;
+            cin >> Devhelp;
+            if (Devhelp==1){
+                for (int i = 0; i < kinematicLine; i++) {
+                    for (int j = 0; j < 8; j++) {
+                        float tempdata = kinematicContent[i][j];
+
+                        cout << tempdata << endl;
+                    }
                     cout << endl;
                 }
             }
-        }
-    if (DevMode) {
-        cout << "Bestatige die Ausgabe der C daten mit 1" << endl;
-        int Devhelp=0;
-        cin >> Devhelp;
-        if (Devhelp==1) {
-            for (int i = 0; i < LineNumber[0]; i++) {
-                for (int j = 0; j < 3; j++) {
-                    cout<< LineContent[i][j] << " ";
+            cout << "Bestatige die Ausgabe der daten aus " << getfolder() <<"\\FallenderTropfen\\"<< FolderNumber <<"\\C mit 1" << endl;
+            Devhelp=0;
+            cin >> Devhelp;
+            if (Devhelp==1) {
+                for (int i = 0; i < LineNumber[0]; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        cout<< LineContent[i][j] << " ";
+                    }
+                    cout << endl;
                 }
-                cout << endl;
+                cout << "Das zweite set an vektordaten" << endl;
+                for (int i = 0; i < LineNumber[1]; i++) {
+                    for (int j = 6; j < 9; j++) {
+                        cout<< LineContent[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << "Das dritte set an vektordaten" << endl;
+                for (int i = 0; i < LineNumber[2]; i++) {
+                    for (int j = 9; j < 12; j++) {
+                        cout<< LineContent[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << "Bestatige die Ausgabe der daten aus " << getfolder() <<"\\FallenderTropfen\\"<< FolderNumber <<"\\U mit 1" << endl;
+                Devhelp=0;
+                cin >> Devhelp;
+                if (Devhelp==1) {
+                    for (int i = 0; i < LineNumber[3]; i++) {
+                        for (int j = 3; j < 6; j++) {
+                            cout<< LineContent[i][j] << " ";
+                        }
+                        cout << endl;
+                    }
+                }
             }
         }
-    }
-       // GeleseneDatenC Classname(LineNumber, LineContentTransfer, kinematicLine, kinematicContentTransfer);
 
-        return 0;
+    }
+    else if (ErrorIsThere==true) {
+        if (DevMode) {
+            cout<< "There was an Error opening a vital File, check the other Error messages for more Information" << endl;
+        }
+    }
+        return 27;
     }
